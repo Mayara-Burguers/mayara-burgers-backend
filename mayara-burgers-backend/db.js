9a706eb950402fc1377d789d29a6d83a068b6d6c
Mayara-Burguers <ph5388446@gmail.com>
2025-07-16 23:16:42 -0300

    Update db.js

--- mayara-burgers-backend/db.js
+++ mayara-burgers-backend/db.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 // Cria um pool de conexões para PostgreSQL usando a URL do ambiente
+// Ele vai procurar pela variável DATABASE_URL que você configurou no Render/Fly.io
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -11,6 +12,7 @@ const pool = new Pool({
 
 console.log("Pool de conexões com o banco de dados (PostgreSQL) criado!");
 
+// Exportamos o objeto pool para ser usado em outros arquivos
 module.exports = {
     query: (text, params) => pool.query(text, params),
     connect: () => pool.connect(),
