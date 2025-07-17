d3c8c5fed251fc2a09809d05b95d42d9b5931d00
Mayara-Burguers <ph5388446@gmail.com>
2025-07-16 22:11:47 -0300

    Update db.js

--- mayara-burgers-backend/db.js
+++ mayara-burgers-backend/db.js
@@ -1,17 +1,13 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-// Cria um "pool" de conexões, que é mais eficiente do que criar uma nova conexão a cada consulta
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-console.log("Pool de conexões com o MySQL criado com sucesso!");
+console.log("Pool de conexões com o banco de dados criado!");
 
 module.exports = pool;
