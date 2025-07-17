4eaddd79c24357d1f509cae782e42e322d30ffc6
Mayara-Burguers <ph5388446@gmail.com>
2025-07-16 23:06:44 -0300

    Update db.js

--- mayara-burgers-backend/db.js
+++ mayara-burgers-backend/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// Cria um pool de conexões para PostgreSQL usando a URL do ambiente
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -8,6 +9,9 @@ const pool = new Pool({
     }
 });
 
-console.log("Pool de conexões com o banco de dados criado!");
+console.log("Pool de conexões com o banco de dados (PostgreSQL) criado!");
 
-module.exports = pool;
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+    connect: () => pool.connect(),
+};
